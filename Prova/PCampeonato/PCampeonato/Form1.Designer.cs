c25c1f6891ce50619605a481598194072d69c331
Alex1Souza <110995334+Alex1Souza@users.noreply.github.com>
2022-11-18 20:37:32 -0300

    Add files via upload
    Signed-off-by: Alex1Souza <110995334+Alex1Souza@users.noreply.github.com>

---
+++ Prova/PCampeonato/PCampeonato/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PCampeonato
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtxtResultado = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtxtResultado
+            // 
+            this.rtxtResultado.Location = new System.Drawing.Point(519, 32);
+            this.rtxtResultado.Name = "rtxtResultado";
+            this.rtxtResultado.Size = new System.Drawing.Size(270, 335);
+            this.rtxtResultado.TabIndex = 1;
+            this.rtxtResultado.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Executar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(861, 505);
+            this.Controls.Add(this.rtxtResultado);
+            this.Controls.Add(this.button1);
+            this.Name = "frmPrincipal";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtxtResultado;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Prova/PCampeonato/PCampeonato/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PCampeonato
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void btnExecutar_Click(object sender, EventArgs e)
+        {
+            int[,] campeonato = new int[2, 3];
+
+            campeonato[0, 0] = 1;
+            campeonato[0, 1] = 3;
+            campeonato[0, 2] = 5;
+            campeonato[1, 0] = 2;
+            campeonato[1, 1] = 3;
+            campeonato[1, 2] = 5;
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        rtxtResultado.Text = "Time:" + campeonato[i, j] + "Gols feitos " + campeonato[i + 1, j + 1] + "Gols recebidos" + campeonato[i + 1, j + 2];
+                    }
+                    else if (i == 1 && j == 0)
+                    {
+                        rtxtResultado.Text = "Time:" + campeonato[i, j] + "Gols feitos " + campeonato[i, j] + campeonato[i, j];
+                    }
+                }
+            }
+
+
+        }
+
+
+    }
+}
+
